0e87cd87aacabfaed7817c088fb8d3e831503309
Khalida Aliyeva <khld.aliyeva@gmail.com>
2022-08-28 14:31:43 +0400

    Add EncryptFragment

---
+++ app/src/main/java/com/hfad/secretmessage/EncryptFragment.kt
@@ -0,0 +1,18 @@
+package com.hfad.secretmessage
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class EncryptFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_encrypt, container, false)
+    }
+
+}
--- app/src/main/java/com/hfad/secretmessage/MessageFragment.kt
+++ app/src/main/java/com/hfad/secretmessage/MessageFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 
 class MessageFragment : Fragment() {
 
@@ -12,7 +14,14 @@ class MessageFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_message, container, false)
+        val view = inflater.inflate(R.layout.fragment_message, container, false)
+        val btnNext = view.findViewById<Button>(R.id.btnNext)
+
+        btnNext.setOnClickListener {
+            view.findNavController().navigate(R.id.action_messageFragment_to_encryptFragment)
+        }
+
+        return view
     }
 
 }

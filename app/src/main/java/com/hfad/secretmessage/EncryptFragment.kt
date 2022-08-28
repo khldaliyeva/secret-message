7a6b7fd3ea60b6a8ef563086dddcd53bbb005f29
Khalida Aliyeva <khld.aliyeva@gmail.com>
2022-08-28 19:57:12 +0400

    Add Safe Args plug-in

--- app/src/main/java/com/hfad/secretmessage/EncryptFragment.kt
+++ app/src/main/java/com/hfad/secretmessage/EncryptFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 
 class EncryptFragment : Fragment() {
@@ -12,7 +13,11 @@ class EncryptFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_encrypt, container, false)
+        val view = inflater.inflate(R.layout.fragment_encrypt, container, false)
+        val message = EncryptFragmentArgs.fromBundle(requireArguments()).message
+        val tvEncryptedText = view.findViewById<TextView>(R.id.tvEncryptedText)
+        tvEncryptedText.text = message.reversed()
+        return view
     }
 
 }
--- app/src/main/java/com/hfad/secretmessage/MessageFragment.kt
+++ app/src/main/java/com/hfad/secretmessage/MessageFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 
@@ -16,9 +17,13 @@ class MessageFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_message, container, false)
         val btnNext = view.findViewById<Button>(R.id.btnNext)
+        val etMessage = view.findViewById<EditText>(R.id.etMessage)
 
         btnNext.setOnClickListener {
-            view.findNavController().navigate(R.id.action_messageFragment_to_encryptFragment)
+            val message = etMessage.text.toString()
+            val action = MessageFragmentDirections
+                .actionMessageFragmentToEncryptFragment(message)
+            view.findNavController().navigate(action)
         }
 
         return view
